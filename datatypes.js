a5da028529eea6525de9dcf5084dc3d7654788a9
anwe712 <ghoshanwesha272@gmail.com>
2023-06-07 12:22:41 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+let a = 345.78
+let b = null 
+let c = BigInt("9032")
+let d = "String is here"
+let e = Symbol("I am a symbol")
+let f = undefined
+let g = true
+console.log( typeof a , b, c, d ,e, f, g)
